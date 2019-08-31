965ae7689aef4a243d4c01b7979aaa0d79ce08ed
thai <thai.do@codelink.io>
2019-08-31 17:27:13 +0700

    Add API to return user rating

--- src/main/java/com/thaido/ratingdataservice/controller/RatingController.java
+++ src/main/java/com/thaido/ratingdataservice/controller/RatingController.java
@@ -1,9 +1,13 @@
 package com.thaido.ratingdataservice.controller;
 
 import com.thaido.ratingdataservice.model.Rating;
+import com.thaido.ratingdataservice.model.UserRatings;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("rating")
@@ -16,5 +20,16 @@ public class RatingController {
       movieId,
       4
     );
+  }
+
+  @RequestMapping("/user/{userId}")
+  public UserRatings getUsersRating(@PathVariable("userId") String userId) {
+    List<Rating> ratings = Arrays.asList(
+      new Rating("1234", 4),
+      new Rating("5678", 5)
+    );
+    UserRatings userRatings = new UserRatings();
+    userRatings.setUserRatings(ratings);
+    return userRatings;
   }
 }
